9d28877329f041d645b70f839ba14c6e06fa85fe
Austin <ahall24@cub.uca.edu>
2020-05-20 19:38:12 -0500

    testing the camers

--- ultra.py
+++ ultra.py
@@ -1,7 +1,8 @@
+import multiprocessing
 import time
 import Adafruit_PCA9685
 import getch
-
+import test_video
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
@@ -19,12 +20,20 @@ def straight():
 def up():
     pwm.set_pwm(11,0,180)
     time.sleep(0.2)
+#test_video.stream()
+def control():
+    while True:
+        #char = input()
+        char = getch.getch()
+        if char == 'd':
+            down()
+        if char == 's':
+            straight()
+        if char  == 'u':
+            up()
 
-while True:
-    char = getch.getch()
-    if char == 'd':
-        down()
-    if char == 's':
-        straight()
-    if char  == 'u':
-        up()
+
+v1=multiprocessing.Process(target=test_video.stream)
+v1.start()
+c1 = multiprocessing.Process(target = control)
+c1.start()
